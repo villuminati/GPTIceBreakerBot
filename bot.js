40194858a8819efaeec166139255bd12907eb5cc
rshm25 <rishi25m@gmail.com>
2023-03-29 16:46:37 +0530

    the bot replies withing thread for existing convos. creates new thread for new messages in channel

--- bot.js
+++ bot.js
@@ -1,5 +1,10 @@
 import dotenv from "dotenv";
-import { Client, GatewayIntentBits } from "discord.js";
+import {
+	Client,
+	GatewayIntentBits,
+	ThreadAutoArchiveDuration,
+	ChannelType,
+} from "discord.js";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
@@ -9,6 +14,7 @@ const client = new Client({
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.DirectMessageTyping,
 	],
 });
 
@@ -20,7 +26,7 @@ const openai = new OpenAIApi(
 
 client.on("messageCreate", async function (message) {
 	if (message.author.bot) return;
-
+	console.lo;
 	try {
 		const response = await openai.createChatCompletion({
 			model: "gpt-3.5-turbo",
@@ -33,9 +39,20 @@ client.on("messageCreate", async function (message) {
 				{ role: "user", content: message.content },
 			],
 		});
-
 		const content = response.data.choices[0].message;
-		return message.reply(content);
+
+		console.log({ message_channel_type: message.channel.type });
+		if (message.channel.type === ChannelType.GuildText) {
+			const discussThread = await message.startThread({
+				name: "Ice Breaker",
+				type: "GUILD_PUBLIC_THREAD",
+				reason: "test",
+				autoArchiveDuration: ThreadAutoArchiveDuration.OneWeek,
+			});
+			return discussThread.send(content);
+		} else if (message.channel.type === ChannelType.PublicThread) {
+			return message.reply(content);
+		}
 	} catch (err) {
 		console.error(err);
 		return message.reply("As an AI robot, I errored out.");
